7799bbadc66f1e5fe9c8ac873d73b0515a669099
cuplprojects <shivom.chandrakala@gmail.com>
2024-09-30 11:57:34 +0530

    development server deployment instructions
    Readme.md file change to have instructions.
    change the vite cofig file to add outDir path for build to the hosted IIS maped folder.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir: '//192.168.1.24/e/hosted apps/ApexERP',
+  },
   plugins: [react()],
 })
