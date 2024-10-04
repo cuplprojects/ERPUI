482d14bf2452ecf8cf95cbe3ecb2decd09844cb7
cuplprojects <shivom.chandrakala@gmail.com>
2024-10-04 12:21:23 +0530

    Update AlertBadge.jsx

--- src/sub-Components/AlertBadge.jsx
+++ src/sub-Components/AlertBadge.jsx
@@ -2,8 +2,23 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { FaCircleCheck } from "react-icons/fa6";
 import { Row, Col } from 'react-bootstrap';
+import themeStore from './../store/themeStore';
+import { useStore } from 'zustand';
 
-const AlertBadge = ({ text, data, onClick, status }) => {
+const AlertBadge = ({ catchNo,alerts, onClick, status }) => {
+
+    //Theme Change Section
+    const { getCssClasses } = useStore(themeStore);
+    const cssClasses = getCssClasses();
+    const customDark = cssClasses[0];
+    const customMid = cssClasses[1];
+    const customLight = cssClasses[2];
+    const customBtn = cssClasses[3];
+    const customDarkText = cssClasses[4];
+    const customLightText = cssClasses[5]
+    const customLightBorder = cssClasses[6]
+    const customDarkBorder = cssClasses[7]
+
     const [visible, setVisible] = useState(true);
     const activeUser = JSON.parse(localStorage.getItem('activeUser'));
     const handleClose = (e) => {
@@ -32,11 +47,14 @@ const AlertBadge = ({ text, data, onClick, status }) => {
             <div
                 className={`badge c-pointer d-flex align-items-center  p-2 me-2 ${getBadgeStyles()}`}
                 onClick={onClick}
-                style={{ width: "200px", background: "", border: "2px solid #FFD964" }}
+                style={{ width: "auto", background: "", border: "2px solid #FFD964" }}
             >
                 <Col lg={10} md={10} xs={10}>
-                    <span className='fs-5 custom-theme-dark-text'>
-                        Catch : {text}
+                    <span className={`fs-5 ${customDarkText}`}>
+                        Catch : {catchNo}
+                    </span>
+                    <span className={`fs-5 ms-2 ${customDarkText}`}>
+                        ({alerts})
                     </span>
                 </Col>
                 {activeUser && activeUser.userId === 'admin' && (
@@ -47,7 +65,7 @@ const AlertBadge = ({ text, data, onClick, status }) => {
                             onClick={handleClose}
                             style={{ color: "inherit" }}
                         >
-                            <span className='bg-white rounded-circle p-1 d-flex justify-content-start  align-items-center'>
+                            <span className='bg-white rounded-circle p-1 d-flex justify-content-start  align-items-center ms-3'>
                                 <FaCircleCheck size={25} color='green' />
                             </span>
                         </Button>
