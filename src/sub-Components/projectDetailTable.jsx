955afc66b59f99af7a2f1fe86b74d8f2bc1d7a3b
cuplprojects <shivom.chandrakala@gmail.com>
2024-11-11 14:04:25 +0530

    fixed newIndex issue and added some trasnlation in process screen

--- src/sub-Components/projectDetailTable.jsx
+++ src/sub-Components/projectDetailTable.jsx
@@ -23,6 +23,7 @@ import { MdPending } from "react-icons/md";// for pending
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";// for completed
 import API from '../CustomHooks/MasterApiHooks/api';
 import { hasPermission } from '../CustomHooks/Services/permissionUtils';
+import { useTranslation } from 'react-i18next';
 
 const { Option } = Select;
 
@@ -31,6 +32,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
     console.log(tableData);
     console.log(processId);
     //Theme Change Section
+    const { t } = useTranslation();
     const { getCssClasses } = useStore(themeStore);
     const cssClasses = getCssClasses();
     const customDark = cssClasses[0];
@@ -240,14 +242,14 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
             responsive: ['sm'],
         },
         {
-            title: 'Sr.No.',
+            title: t("srNo"),
             key: 'srNo',
             align: "center",
             render: (_, __, index) => ((currentPage - 1) * pageSize) + index + 1,
             responsive: ['sm'],
         },
         {
-            title: 'Catch No.',
+            title: t("catchNo"),
             dataIndex: 'catchNumber',
             key: 'catchNumber',
             align: 'center',
@@ -325,21 +327,21 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
             ),
         },
         {
-            title: 'Quantity',
+            title: t("quantity"),
             dataIndex: 'quantity',
             key: 'quantity',
             align: 'center',
             sorter: (a, b) => a.quantity - b.quantity,
         },
         ...(columnVisibility['Interim Quantity'] && hasFeaturePermission(7) ? [{
-            title: 'Interim Quantity',
+            title: t("interimQuantity"),
             dataIndex: 'interimQuantity',
             align: 'center',
             key: 'interimQuantity',
             sorter: (a, b) => a.interimQuantity - b.interimQuantity,
         }] : []),
         ...(columnVisibility.Remarks ? [{
-            title: 'Remarks',
+            title: t("remarks"),
             dataIndex: 'remarks',
             key: 'remarks',
             align: 'center',
@@ -348,9 +350,9 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
 
         ...(columnVisibility['Team Assigned'] && hasFeaturePermission(5) ? [{
 
-            title: 'Team Assigned',
+            title: t("teamAssigned"),
             dataIndex: 'teamUserNames',
-            width: '20%',
+            // width: '20%',
             align: 'center',
             key: 'teamUserNames',
 
@@ -365,15 +367,15 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
 
         }] : []),
         ...(columnVisibility['Course'] && hasFeaturePermission(13) ? [{
-            title: 'Course',
+            title: t("course"),
             dataIndex: 'course',
-            width: '20%',
+            // width: '20%',
             align: 'center',
             key: 'course',
             sorter: (a, b) => a.course - b.course,
         }] : []),
         ...(columnVisibility['Subject'] && hasFeaturePermission(14) ? [{
-            title: 'Subject',
+            title: t("subject"),
             dataIndex: 'subject',
             width: '20%',
             align: 'center',
@@ -381,7 +383,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
             sorter: (a, b) => a.subject - b.subject,
         }] : []),
         ...(columnVisibility['Paper'] && hasFeaturePermission(15) ? [{
-            title: 'Paper',
+            title: t("questionPaper"),
             dataIndex: 'paper',
             width: '20%',
             align: 'center',
@@ -389,7 +391,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
             sorter: (a, b) => a.paper - b.paper,
         }] : []),
         {
-            title: 'Status',
+            title: t("status"),
             dataIndex: 'status',
             key: 'status',
             align: 'center',
@@ -398,7 +400,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                     return <span>Invalid Data</span>; // Fallback for invalid data
                 }
 
-                const statusSteps = ["Pending", "Started", "Completed"];
+                const statusSteps = [t("pending"), t("started"), t("completed")];
                 const initialStatusIndex = text !== undefined ? text : 0;
 
                 const hasAlerts = Boolean(record.alerts?.length);
@@ -410,7 +412,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
 
                 // Check if 'Select Machine' is required (i.e., permission granted)
                 const hasSelectMachinePermission = hasFeaturePermission(10); // Check if the user has Select Machine permission
-
+                const canBeCompleted = record.interimQuantity === record.quantity;
                 // The status can only be changed if:
                 // 1. The Select Machine is assigned (if permission for Select Machine exists)
                 // 2. OR The Zone and Team are assigned (if permission for Select Machine doesn't exist)
@@ -440,7 +442,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                                     status,
                                     color: index === 0 ? "red" : index === 1 ? "blue" : "green"
                                 }))}
-                                disabled={!canChangeStatus || (newIndex === 2 && !canBeCompleted)} // Disable the toggle if status can't be changed (based on Select Machine or Zone/Team)
+                                disabled={!canChangeStatus || (initialStatusIndex === 2 && !canBeCompleted)} // Disable the toggle if status can't be changed (based on Select Machine or Zone/Team)
                             />
                         )}
                     </div>
@@ -651,34 +653,34 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
         <Menu>
             {hasFeaturePermission(3) && !isCompleted && (
                 <Menu.Item onClick={() => handleDropdownSelect('Alarm')}>
-                    Alarm
+                    {t("alarm")}
                 </Menu.Item>
             )}
             {hasFeaturePermission(7) && !isCompleted && isStarted && (
                 <Menu.Item onClick={() => handleDropdownSelect('Interim Quantity')}>
-                    Interim Quantity
+                    {t("interimQuantity")}
                 </Menu.Item>
             )}
             {!isCompleted && (
                 <Menu.Item onClick={() => handleDropdownSelect('Remarks')}>
-                    Remarks
+                    {t("remarks")}
                 </Menu.Item>
             )}
-            <Menu.Item onClick={() => setColumnModalShow(true)}>Columns</Menu.Item>
+            <Menu.Item onClick={() => setColumnModalShow(true)}>{t("columns")}</Menu.Item>
             {hasFeaturePermission(4) && (
                 <Menu.Item onClick={() => handleDropdownSelect('Select Zone')}
 
-                    disabled={selectedRowKeys.length === 0}>Select Zone</Menu.Item>
+                    disabled={selectedRowKeys.length === 0}>{t("selectZone")}</Menu.Item>
             )}
             {hasFeaturePermission(10) && (
                 <Menu.Item onClick={() => handleDropdownSelect('Select Machine')}
 
-                    disabled={selectedRowKeys.length === 0}>Select Machine</Menu.Item>
+                    disabled={selectedRowKeys.length === 0}>{t("selectMachine")}</Menu.Item>
             )}
             {hasFeaturePermission(5) && (
                 <Menu.Item onClick={() => handleDropdownSelect('Assign Team')}
 
-                    disabled={selectedRowKeys.length === 0}>Assign Team</Menu.Item>
+                    disabled={selectedRowKeys.length === 0}>{t('assignTeam')}</Menu.Item>
             )}
         </Menu>
     );
@@ -690,11 +692,11 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
         showSizeChanger: true,
         onShowSizeChange: (current, size) => setPageSize(size),
         onChange: (page) => setCurrentPage(page),
-        showTotal: (total) => `Total ${total} items`,
-        locale: { items_per_page: "Rows" }, // Removes the "/page" text
+        showTotal: (total) => `${t("total")} ${total} ${t("items")}`,
+        locale: { items_per_page: `${t('rows')}` }, // Removes the "/page" text
         pageSizeRender: (props) => (
             <div style={{ display: "flex", alignItems: "center" }}>
-                <span style={{ marginRight: 8 }} className=''>Limit Rows:</span>
+                <span style={{ marginRight: 8 }} className=''>{t("limitRows")}:</span>
                 {props}
             </div>
         ),
@@ -732,7 +734,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                                         checked={hideCompleted}
                                         onChange={handleToggleChange}
                                     />
-                                    <span className='ms-2'>Hide Completed</span>
+                                    <span className='ms-2'>{t("hideCompleted")}</span>
                                 </Menu.Item>
 
                                 <Menu.Divider />
@@ -742,7 +744,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                                         onChange={() => setShowOnlyCompletedPreviousProcess(!showOnlyCompletedPreviousProcess)}
                                     />
                                     {/* <span className='ms-2'>{previousProcess} Completed</span> */}
-                                    <span className='ms-2'>Previous  Completed</span>
+                                    <span className='ms-2'>{t("previousCompleted")}</span>
                                 </Menu.Item>
                                 <Menu.Divider />
 
@@ -751,7 +753,7 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                                         checked={showOnlyAlerts}
                                         onChange={() => setShowOnlyAlerts(!showOnlyAlerts)}
                                     />
-                                    <span className='ms-2'>Catches With Alerts</span>
+                                    <span className='ms-2'>{t("catchesWithAlerts")}</span>
                                 </Menu.Item>
 
                                 <Menu.Divider />
@@ -760,13 +762,13 @@ const ProjectDetailsTable = ({ tableData, fetchTransactions, setTableData, proje
                                         checked={showOnlyRemarks}
                                         onChange={() => setShowOnlyRemarks(!showOnlyRemarks)}
                                     />
-                                    <span className='ms-2'>Catches With Remarks</span>
+                                    <span className='ms-2'>{t("catchesWithRemarks")}</span>
                                 </Menu.Item>
 
                                 <Menu.Divider />
 
                                 <Menu.Item onClick={(e) => e.stopPropagation()}> {/* Add this */}
-                                    <span>Limit Rows:</span>
+                                <span>{t("limitRows")}:</span>
                                     <Select
                                         value={pageSize}
                                         style={{ width: 60 }}
